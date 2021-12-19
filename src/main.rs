caf3d7ce5fd56cf197fa8a9ae49b44269b52c7bd
huud-e <huud.duu@gmail.com>
2021-12-19 17:38:33 +0100

    Setup the web server

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, Duration};
+mod models;
+mod config;
 
-#[actix_web::main]
-async fn main() {
-    HttpServer::new(|| {
-        App::new().route("/", web::get().to(|| HttpResponse::Ok()))
-    })
-    .workers(4); // <- Start 4 workers
+use crate::models::Status;
+use actix_web::{HttpServer, App, web, Responder};
+use std::io;
+use dotenv::dotenv;
+
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok()
+        .json(Status { status: "UP".to_string() })
 }
 
-fn my_handler() -> impl Responder {
-    std::thread::sleep(Duration::from_secs(5)); // <-- Bad practice! Will cause the current worker thread to hang!
-    "response"
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    println!("Starting server at http://{}:{}", config.server.host, config.server.port);
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(status))
+    })
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
+    .run()
+    .await
 }
