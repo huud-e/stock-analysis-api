e63aad0b88bf4acd082261ccfa0954977079dd5c
huud-e <huud.duu@gmail.com>
2021-12-19 17:02:48 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Duration};
+
+#[actix_web::main]
+async fn main() {
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(|| HttpResponse::Ok()))
+    })
+    .workers(4); // <- Start 4 workers
+}
+
+fn my_handler() -> impl Responder {
+    std::thread::sleep(Duration::from_secs(5)); // <-- Bad practice! Will cause the current worker thread to hang!
+    "response"
+}
